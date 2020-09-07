6b2defd6ee60e7409c3b724045f706dc9c4d10be
takamuraTK <tirpitz.zzz@outlook.jp>
2020-09-08 01:37:25 +0900

    Add login

--- app/pages/users/login.vue
+++ app/pages/users/login.vue
@@ -2,17 +2,41 @@
   <v-container>
     <h2>ログイン</h2>
     <v-form ref="form" v-model="valid" lazy-validation>
-      <v-text-field v-model="email" :rules="emailRules" label="メールアドレス" required></v-text-field>
-      <v-text-field v-model="password" :rules="passwordRules" label="パスワード" required></v-text-field>
-      <v-btn :disabled="!valid" color="success" class="mr-4" @click="login">ログイン</v-btn>
+      <v-text-field v-model="email" :rules="[required, emailRule]" label="メールアドレス"></v-text-field>
+      <v-text-field v-model="password" :rules="[required, passwordRule]" label="パスワード"></v-text-field>
+      <v-btn :disabled="!valid" color="success" class="mt-4" @click="login">ログイン</v-btn>
     </v-form>
   </v-container>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      valid: true,
+      email: "",
+      password: "",
+    };
+  },
   methods: {
-    async login() {},
+    async login() {
+      if (this.$refs.form.validate()) {
+        const response = await this.$authaxios.$post(
+          "/accounts:signInWithPassword",
+          {
+            email: this.email,
+            password: this.password,
+            returnSecureToken: true,
+          }
+        );
+        console.log(response);
+      }
+    },
+    required: (value) => !!value || "必ず入力してください",
+    emailRule: (value) =>
+      /.+@.+\..+/.test(value) || "正しいメールアドレスを入力してください",
+    passwordRule: (value) =>
+      value.length >= 6 || "パスワードは6字以上で入力してください",
   },
 };
 </script>
